552d93055288579dcddbd713b97f20a73d5319cf
Freddie Mac - KD5FMU <fred.mcguire.jr@gmail.com>
2024-11-14 16:54:33 -0600

    Update enable_tones.sh

--- enable_tones.sh
+++ enable_tones.sh
@@ -3,7 +3,7 @@
 AST_CFG='/etc/asterisk'
 
 # Backup the current configuration
-sudo cp $AST_CFG/rpt.conf $AST_CFG/rpt.conf.backup
+# sudo cp $AST_CFG/rpt.conf $AST_CFG/rpt.conf.backup
 
 # Enable all courtesy tones (local and remote)
 sudo sed -i 's/^telemdefault=0/telemdefault=2/g' $AST_CFG/rpt.conf
